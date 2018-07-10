d81e755ab292342cb9ddebd482f799c7fecb4489
Bryan-Farinango <38628690+Bryan-Farinango@users.noreply.github.com>
2018-07-10 09:08:22 -0500

    Add files via upload

---
+++ PruebaBimestre/src/pruebabimestre/Clientes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebabimestre;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Clientes {
+    String nombre;
+    String cedula;
+
+    public Clientes(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+    
+    
+}
